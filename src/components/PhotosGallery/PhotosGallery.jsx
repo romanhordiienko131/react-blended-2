df9500b0097905acf1954bd927405212429f52ae
vitalii-pc <alievruslan1818@gmail.com>
2024-09-14 11:59:15 +0200

    gat photos

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,17 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid, PhotosGalleryItem } from '..';
+export const PhotosGallery = ({ photos }) => {
+  console.log(photos);
+  return (
+    <Grid>
+      {photos.map(photo => (
+        <PhotosGalleryItem
+          key={photo.id}
+          id={photo.id}
+          avg_color={photo.avg_color}
+          alt={photo.alt}
+          src={photo.src}
+        />
+      ))}
+    </Grid>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,35 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Text, Form, PhotosGallery } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [photos, setPhotos] = useState([]);
+  const [error, setError] = useState(null);
+  const onSubmit = value => {
+    setSearchValue(value);
+  };
+
+  useEffect(() => {
+    if (!searchValue) return;
+
+    const getData = async () => {
+      try {
+        const data = await getPhotos(searchValue, 1);
+        setPhotos(data.photos);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    getData();
+  }, [searchValue]);
+
   return (
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={onSubmit} />
+      <PhotosGallery photos={photos} />
+      {error && <Text textAlign="center">{error}</Text>}
     </>
   );
 };
